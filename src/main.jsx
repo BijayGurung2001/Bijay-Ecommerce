ce4a02b32fa3df7d73c652c9d48445fdcc2b1544
BijayGurung2001 <85681343+BijayGurung2001@users.noreply.github.com>
2024-02-22 13:07:23 +0545

    update in main dunction

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css";
-import {ProductProvider} from './Context/ProductContext.jsx';
+import ProductProvider from './Context/ProductContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
