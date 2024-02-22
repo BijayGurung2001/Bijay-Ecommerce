0f56879bf209f2b32ee5baedf08097ed3403469b
BijayGurung2001 <85681343+BijayGurung2001@users.noreply.github.com>
2024-02-22 13:07:40 +0545

    creating single page layout

--- src/components/ProductLayout.jsx
+++ src/components/ProductLayout.jsx
@@ -1,21 +1,29 @@
 import React, { useContext } from 'react'
-import ProductContext from '../Context/ProductContext'
+import { ProductContext } from '../Context/ProductContext'
 import { Link } from 'react-router-dom'
 
+
 const ProductLayout = () => {
-    const {ProductData,isLoading ,isError}=useContext(ProductContext)
+    const {ProductData, isError, isLoading}=useContext(ProductContext);
+    console.log(ProductData);
+   
+      if (!ProductData || ProductData.length === 0) {
+        return <h1>No data available</h1>;
+      }
+    
   return (
     <div>
         {isLoading && <h1>Loading</h1>}
         {!isLoading && isError && <h1>Error Occured</h1>}
-        {!isLoading && !isError && ProductData.length>0 && 
+        {!isLoading && !isError && ProductData.length>0 &&
         ProductData.map((product)=>{
             return(
-                <div>
+                <div key={product.id}>
                     <Link to={(`/product/${product.id}`)}>
-                        <img src={product.image} />
+                        <img src={product.image} alt={product.title}/>
                         <br/ >
                         <h2>{product.title}</h2>
+                        
                         <h3>{product.price}</h3>
                         <button>Add to card</button>
                     </Link>
